79bc192d7e5006c5ac5fc3d4411cd1e01d222712
shen-yang-tw <shen.yang.work.tw@gmail.com.tw>
2018-03-09 15:07:58 +0800

    編修JS

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@ $(document).ready(function() {
     $('.font_resize').removeClass('font_large').removeClass('font_medium');
   });
 
+  //Remove parent if child empty
+  $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty").parent(".uk-overlay-panel").remove();
+  //Remove if empty
+  $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty").remove();
+
+  //show topbar on scroll, (body) not working
+  $(window).scroll(function() {
+    $("#header").removeClass("opacity0").addClass("uk-animation-fade opacity1");
+  });
+
+  //Get current year
+  $(".year").text((new Date).getFullYear());
+
   /*remote URL to show content in the same modal*/
   $("#btn1").click(function() {
     $('#myModal').on('shown.bs.modal', function(e) {
