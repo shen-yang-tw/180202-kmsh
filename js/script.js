703511196bfd2c2e60b773fac9692ad60edb3271
shen-yang-tw <shen.yang.work.tw@gmail.com.tw>
2018-03-23 16:48:00 +0800

    首頁-2版4型完成

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+// fix 100 vh in Android Chrome
+var vhFix = new VHChromeFix([{
+  selector: '.fullViewH',
+  vh: 100
+}]);
+
+//FontAwesome load at the beginning
+FontAwesomeConfig = { searchPseudoElements: true };
+
 $(document).ready(function() {
+
+  //Get current year
+  $(".year").text((new Date).getFullYear());
+
+  //FontAwesome load on document ready
+  // window.FontAwesomeConfig.searchPseudoElements = true;
+
   //font resize
   $('.large>a').click(function() {
     $('.font_resize').removeClass('font_medium').addClass('font_large');
@@ -15,16 +31,8 @@ $(document).ready(function() {
   //Remove if empty
   $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty").remove();
 
-  //show topbar on scroll, (body) not working
-  $(window).scroll(function() {
-    $("#header").removeClass("opacity0").addClass("uk-animation-fade opacity1");
-  });
-
-  //Get current year
-  $(".year").text((new Date).getFullYear());
-
   //calendar
-  var sampleEvents = {
+  var A1 = {
     "monthly": [{
         "id": 1,
         "name": "大得象功個土會代之題速越眼",
@@ -44,11 +52,59 @@ $(document).ready(function() {
       }
     ]
   };
-  $('#calendar').monthly({
+
+  var A2 = {
+    "monthly": [{
+        "id": 1,
+        "name": "大得象功個土會代之題速越眼",
+        "startdate": "2018-03-20",
+        "enddate": "2018-03-21",
+        "starttime": "12:00",
+        "endtime": "2:00",
+        "color": "#3ab54a",
+        "url": ""
+      },
+      {
+        "id": 2,
+        "name": "本日閉館",
+        "startdate": "2018-03-19",
+        "color": "#ff6a00",
+        "url": ""
+      }
+    ]
+  };
+
+  $('#calendarA1').monthly({
     mode: 'event',
     dataType: 'json',
     // xmlUrl: 'events.xml',
-    events: sampleEvents
+    events: A1
+  });
+
+  $('#calendarA2').monthly({
+    mode: 'event',
+    dataType: 'json',
+    // xmlUrl: 'events.xml',
+    events: A2
+  });
+
+  $('#calendarB1').monthly({
+    mode: 'event',
+    dataType: 'json',
+    // xmlUrl: 'events.xml',
+    events: A1
+  });
+
+  $('#calendarB2').monthly({
+    mode: 'event',
+    dataType: 'json',
+    // xmlUrl: 'events.xml',
+    events: A1
+  });
+
+  //Sticky card toggleClass class when open/close
+  $('.card_sticky').on('toggle.uk.accordion', function() {
+    $(this).toggleClass('open');
   });
 
 });
